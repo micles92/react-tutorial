0f7c253062bd417cce74627e739cb3951be672d3
E050AQ <mlesniak@genuitek.com>
2019-12-02 23:41:34 +0100

    Przekazywanie Referencji Metody pomiedzy komponentami

--- src/App.js
+++ src/App.js
@@ -10,28 +10,36 @@ class App extends Component {
             {name: 'Stephanie', age: 26}
         ],
         otherState: 'some other value'
-    }
+    };
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         //DONT DO THIS, REACT CANT RECONIZE THIS AND WON'T PICK UP ANY ACTION ->  this.state.persons[0].name = 'Maxsimus';
         this.setState({
             persons: [
-                {name: 'Maximilian', age: 28},
+                {name: newName, age: 28},
                 {name: 'Manu', age: 29},
                 {name: 'Stephanie', age: 27}
             ]
         })
-    }
+    };
 
 
     render() {
         return (
             <div className="App">
                 <h1>Hi, Im a react app</h1>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <button onClick={ () =>this.switchNameHandler('MAXISU@!')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}>My Hobbies: Racing</Person>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click = {this.switchNameHandler.bind(this, 'Max!')}
+                />
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
             </div>
         );
     }
