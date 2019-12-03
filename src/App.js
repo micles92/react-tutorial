fc3f6e080d5c0189261c28db8b530a085f7357ad
E050AQ <mlesniak@genuitek.com>
2019-12-03 22:36:00 +0100

    Nadawanie styli w React inline i osobno

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
 class App extends Component {
     state = {
         persons: [
@@ -23,19 +24,39 @@ class App extends Component {
         })
     };
 
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: 'Max', age: 28},
+                {name: event.target.value, age: 29},
+                {name: 'Stephanie', age: 27}
+            ]
+        })
+    };
 
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1x solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 <h1>Hi, Im a react app</h1>
-                <button onClick={ () =>this.switchNameHandler('MAXISU@!')}>Switch Name</button>
+                <button style={style}
+                        onClick={() => this.switchNameHandler('MAXISU@!')}>Switch Name
+                </button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age}>My Hobbies: Racing</Person>
                 <Person
                     name={this.state.persons[1].name}
                     age={this.state.persons[1].age}
-                    click = {this.switchNameHandler.bind(this, 'Max!')}
+                    click={this.switchNameHandler.bind(this, 'Max!')}
+                    changed={this.nameChangedHandler}
                 />
                 <Person
                     name={this.state.persons[2].name}
